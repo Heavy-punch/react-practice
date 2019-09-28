1389f164c744a3cd45f35d062c29f903decd3386
nguyen.nguyenxuan <nguyen.nguyenxuan@gmail.com>
2019-09-29 00:07:22 +0700

    commit

---
+++ sourcecode/public/scripts/app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("hello wolrd");
+
+var tmp = React.createElement(
+  "h1",
+  null,
+  "noi dung o day"
+);
---
+++ sourcecode/src/app.js
@@ -0,0 +1,4 @@
+console.log("hello wolrd");
+
+
+var tmp = <h1>noi dung o day</h1>;
